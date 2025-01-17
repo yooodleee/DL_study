9df47066de2be8ad489a8653e9aed88eefbfcaef
yooodleee <accia25@naver.com>
2025-01-17 17:32:44 +0900

    compute the log-Mel spectrogram

--- whisper/whisper/audio.py
+++ whisper/whisper/audio.py
@@ -118,3 +118,49 @@ def mel_filters(device, n_mels: int) -> torch.Tensor:
         return torch.from_numpy(f[f"mel_{n_mels}"]).to(device)
 
 
+def log_mel_spectrogram(audio: Union[str, np.ndarray, torch.Tensor],
+                        n_mels: int = 80,
+                        padding: int = 0,
+                        device: Optional[Union[str, torch.device]] = None):
+    """
+    Compute the log-Mel spectrogram of
+
+    Parameters:
+        audio(Union[str, np.ndarray, torch.Tensor], shape = (*)):
+            The path to audio or either a NumPy array or Tensor containing 
+                the audio waveform in 16 KHz.
+        n_mels(int):
+            The number of Mel-frequency filters, only 80 and 128 are supported
+        padding(int):
+            Number of zero samples to pad to the right
+        device(Optional[Union[str, torch.device]]):
+            If given, the audio tensor is moved to this device before STFT
+
+    Returns:
+        torch.Tensor, shape = (n_mels, n_frames):
+            A Tensork that contains the Mell spectrogram
+    """
+    if not torch.is_tensor(audio):
+        if isinstance(audio, str):
+            audio = load_audio(audio)
+        audio = torch.from_numpy(audio)
+    
+    if device is not None:
+        audio = audio.to(device)
+    if padding > 0:
+        audio = F.pad(audio, (0, padding))
+    window = torch.hann_window(N_FFT).to(audio.device)
+    stft = torch.stft(audio,
+                      N_FFT,
+                      HOP_LENGTH,
+                      window=window,
+                      return_complex=True)
+    magnitudes = stft[..., :-1].abs() ** 2
+
+    filters = mel_filters(audio.device, n_mels)
+    mel_spec = filter @ magnitudes
+
+    log_spec = torch.clamp(mel_spec, min=1e-10).log10()
+    log_spec = torch.maximum(log_spec, log_spec.max() - 8.0)
+    log_spec = (log_spec + 4.0) / 4.0
+    return log_spec
