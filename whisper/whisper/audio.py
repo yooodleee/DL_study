d4b78402c5e063c31ecbbfbb24fb4b827ec1a460
yooodleee <accia25@naver.com>
2025-01-17 17:18:58 +0900

    pad or trim the audio array to N_SAMPLES, as expected by the encoder

--- whisper/whisper/audio.py
+++ whisper/whisper/audio.py
@@ -64,3 +64,36 @@ def load_audio(file: str, sr: int = SAMPLE_RATE):
         out, np.int16).flatten().astype(np.float32) / 32768.0
 
 
+def pad_or_trim(array,
+                length: int = N_SAMPLES,
+                *,
+                axis: int = -1):
+    """
+    Pad or trim the audio array to N_SAMPLES, as expected by the encoder.
+    """
+    if torch.is_tensor(array):
+        if array.shape[axis] > length:
+            array = array.index_select(dim=axis,
+                                       index=torch.arange(length,
+                                                          device=array.device))
+        
+        if array.shape[axis] < length:
+            pad_widths = [(0, 0)] * array.ndim
+            pad_widths[axis] = (0, length - array.shape[axis])
+            array = F.pad(array,
+                          [pad for sizes in pad_widths[::-1]
+                           for pad in sizes])
+        
+    else:
+        if array.shape[axis] > length:
+            array = array.take(indices=range(length),
+                               axis=axis)
+            
+        if array.shape[axis] < length:
+            pad_widths = [(0, 0)] * array.ndim
+            pad_widths[axis] = (0, length - array.shape[axis])
+            array = np.pad(array, pad_widths)
+        
+    return array
+
+
