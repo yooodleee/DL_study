0d81efa42c567e2c8943e7efa1dd2f6f503105ac
yooodleee <accia25@naver.com>
2025-01-17 18:35:10 +0900

    create MaximumLikelihoodRanker class

--- whisper/whisper/decoding.py
+++ whisper/whisper/decoding.py
@@ -235,3 +235,34 @@ class SequenceRanker:
         raise NotImplementedError
 
 
+class MaximumLikelihoodRanker(SequenceRanker):
+    """
+    Select the sample with the highest log probabilities, penalized using
+        either a simple length normalization of Google NMT paper's length
+        penalty.
+    """
+
+    def __init__(self, length_penalty: Optional[float]):
+        self.length_penalty = length_penalty
+    
+    def rank(
+            self,
+            tokens: List[List[Tensor]],
+            sum_logprobs: List[List[float]]):
+        def scores(logprobs, lengths):
+            result = []
+            for logprob, length in zip(logprobs, lengths):
+                if self.length_penalty is None:
+                    penalty = length
+                else:
+                    # from the Google NMT paper
+                    penalty = ((5 + length) / 6) ** self.length_penalty
+                result.append(logprob / penalty)
+            return result
+        
+        # get the sequence with the highest score
+        lengths = [[len(t) for t in s] for s in tokens]
+        return [np.argmax(scores(p, l))
+                for p, l in zip(sum_logprobs, lengths)]
+    
+
