72f6236f2b503f8c785420ebc8a861fb5c1cc4b7
yooodleee <accia25@naver.com>
2025-01-17 18:50:47 +0900

    create GreedyDecoder class

--- whisper/whisper/decoding.py
+++ whisper/whisper/decoding.py
@@ -326,3 +326,37 @@ class TokenDecoder:
         raise NotImplementedError
 
 
+class GreedyDecoder(TokenDecoder):
+    def __init__(self, temperature: float, eot: int):
+        self.temperature = temperature
+        self.eot = eot
+    
+    def update(
+            self,
+            tokens: Tensor,
+            logits: Tensor,
+            sum_logprobs: Tensor) -> Tuple[Tensor, bool]:
+        
+        if self.temperature == 0:
+            next_tokens = logits.argmax(dim=-1)
+        else:
+            next_tokens = Categorical(logits=logits 
+                                      / self.temperature).sample()
+        
+        logprobs = F.log_softmax(logits.float(), dim=-1)
+        current_logprobs = logprobs[torch.arange(logprobs.shape[0]), 
+                                    next_tokens]
+        sum_logprobs += current_logprobs * (tokens[:, -1] != self.eot)
+
+        next_tokens[tokens[:, -1] == self.eot] = self.eot
+        tokens = torch.cat([tokens, next_tokens[:, None]], dim=-1)
+
+        completed = (tokens[:, -1] == self.eot).all()
+        return tokens, completed
+    
+    def finalize(self, tokens: Tensor, sum_logprobs: Tensor):
+        # make sure each sequence has at least one EOT token at the end
+        tokens = F.pad(tokens, (0, 1), value=self.eot)
+        return tokens, sum_logprobs.tolist()
+
+
