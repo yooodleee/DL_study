ea86c494bb817fd40acd111f32f478cd91c931ed
yooodleee <accia25@naver.com>
2025-01-17 18:44:09 +0900

    create TokenDecoder class

--- whisper/whisper/decoding.py
+++ whisper/whisper/decoding.py
@@ -266,3 +266,63 @@ class MaximumLikelihoodRanker(SequenceRanker):
                 for p, l in zip(sum_logprobs, lengths)]
     
 
+class TokenDecoder:
+    def reset(self):
+        """
+        Initialize any stateful variable for decoding a new sequence
+        """
+    
+    def update(
+            self,
+            tokens: Tensor,
+            logits: Tensor,
+            sum_logprobs: Tensor) -> Tuple[Tensor, bool]:
+        """
+        Specify how to select the next token, based on the current 
+            trace and logits.
+
+        Parameters:
+            tokens(Tensor, shape = (n_batch, current_sequence_length)):
+                all tokens in the context so far, including the prefix and
+                sot_sequence tokens
+            logits(Tensor, shape = (n_batch, vocab_size)):
+                per-token logits of the probability distribution at the 
+                    current step
+                sum_logprobs(Tensor, shape = (n_batch)):
+                    cumulative log probabilities for each sequence
+
+        Returns:
+            tokens(Tensor, shape = (n_batch, current_sequence_length + 1)):
+                the tokens, appended with the selected next token
+            completed(bool):
+                True if all sequences has reached the end of text
+        """
+        raise NotImplementedError
+    
+    def finalize(
+            self,
+            tokens: Tensor,
+            sum_logprobs: Tensor) -> Tuple[Sequence[Sequence[Tensor]],
+                                           List[List[float]]]:
+        """
+        Finalize search and return the final candidate sequences
+
+        Parameters:
+            tokens(Tensor, shape = (n_audio, n_group, 
+            current_sequence_length)):
+                all tokens in the context so far, including the prefix and 
+                    sot sequence
+            sum_logprobs(Tensor, shape = (n_audio, n_group)):
+                cumulative log probabilities for each sequence
+        
+        Returns:
+            tokens(Sequence[Sequence[Tensor]], length = n_audio):
+                sequence of Tensors containing candidate token sequences,
+                    for each audio input
+            sum_logprobs(List[List[float]], length = n_audio):
+                sequence of cumulative log probabilities corresponding 
+                    to the above
+        """
+        raise NotImplementedError
+
+
