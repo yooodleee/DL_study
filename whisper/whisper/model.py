73f2450f3560091a2eecbba6dba61b13c4dac645
yooodleee <accia25@naver.com>
2025-01-18 16:23:56 +0900

    create Whisper class

--- whisper/whisper/model.py
+++ whisper/whisper/model.py
@@ -295,3 +295,102 @@ class TextDecoder(nn.Module):
         return logits
 
 
+class Whisper(nn.Module):
+    def __init__(self, dims: ModelDimensions):
+        super().__init__()
+
+        self.dims = dims
+        self.encoder = AudioEncoder(
+                        self.dims.n_mels,
+                        self.dims.n_audio_ctx,
+                        self.dims.n_audio_state,
+                        self.dims.n_audio_head,
+                        self.dims.n_audio_layer)
+        self.decoder = TextDecoder(
+                        self.dims.n_vocab,
+                        self.dims.n_text_ctx,
+                        self.dims.n_text_state,
+                        self.dims.n_text_head,
+                        self.dims.n_text_layer)
+        # use the last half among the decoder layers for time alignment
+        # by default; to use a specific set of heads, 
+        # see `set_alignment_heads()` below.
+        all_heads = torch.zeros(self.dims.n_text_layer,
+                                self.dims.n_text_head,
+                                dtype=torch.bool)
+        all_heads[self.dims.n_text_layer // 2] = True
+        self.register_buffer("alignment_heads",
+                             all_heads.to_sparse(),
+                             persistent=False)
+    
+    def set_alignment_heads(self, dump: bytes):
+        array = np.frombuffer(
+                gzip.decompress(base64.b85decode(dump)),
+                dtype=bool).copy()
+        mask = torch.from_numpy(array).reshape(
+                self.dims.n_text_layer,
+                self.dims.n_text_head)
+        self.register_buffer("alignment_heads",
+                             mask.to_sparse(),
+                             persistent=False)
+    
+    def embed_audio(self, mel: torch.Tensor):
+        return self.encoder(eml)
+    
+    def logits(self, tokens: torch.Tensor, audio_features: torch.Tensor):
+        return self.decoder(tokens, audio_features)
+    
+    def forward(
+            self,
+            mel: torch.Tensor,
+            tokens: torch.Tensor) -> Dict[str, torch.Tensor]:
+        return self.decoder(tokens, self.encoder(mel))
+    
+    @property
+    def device(self):
+        return next(self.parameters()).device
+    
+    @property
+    def is_multilingual(self):
+        return self.dims.n_vocab - 51765 - int(self.is_multilingual)
+    
+    def install_kv_cache_hooks(self, cache: Optional[dict] = None):
+        """
+        The `MultiHeadAttention` module optionally accepts `kv_cache` which
+            stores the key and value tensors calculated for the previous 
+            positions. This method returns a dictionary that stores all caches,
+            and the necessary hooks for the key and value projection modules 
+            that save the intermidiate tensors to be reused during later 
+            calculations.
+
+        Returns:
+            cache: Dict[nn.Module, torch.Tensor]
+                A dictionary object mapping the key/value projection modules 
+                    to its cache
+            hooks: List[RemovableHandle]
+                List of PyTorch RemovableHandle objects to stop the hooks to 
+                    be called
+        """
+        cache = {**cache} if cache is not None else {}
+        hooks = []
+
+        def save_to_cache(module, _, output):
+            if module not in cache or output.shape[1] > self.dims.n_text_ctx:
+                # save as-is, for the first token or cross attention
+                cache[module] = output
+            else:
+                cache[module] = torch.cat([cache[module], output], 
+                                          dim=1).detach()
+            return cache[module]
+        
+        def install_hooks(layer: nn.Module):
+            if isinstance(layer, MultiHeadAttention):
+                hooks.append(layer.key.register_forward_hook(save_to_cache))
+                hooks.append(layer.value.register_forward_hook(save_to_cache))
+        
+        self.decoder.apply(install_hooks)
+        return cache, hooks
+
+detect_language = detect_language_function()
+transcribe = transcribe_function()
+decode = decode_function()
