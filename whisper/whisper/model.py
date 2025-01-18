1f5784329b3a7a24fb749709ecda81daa5dddf97
yooodleee <accia25@naver.com>
2025-01-18 15:19:11 +0900

    create AudioEncoder class

--- whisper/whisper/model.py
+++ whisper/whisper/model.py
@@ -197,3 +197,49 @@ class ResidualAttentionBlock(nn.Module):
         return x
     
 
+class AudioEncoder(nn.Module):
+    def __init__(
+            self,
+            n_mels: int,
+            n_ctx: int,
+            n_state: int,
+            n_head: int,
+            n_layer: int):
+        super().__init__()
+        
+        self.conv1 = Conv1d(n_mels,
+                            n_state,
+                            kernel_size=3,
+                            padding=1)
+        self.conv2 = Conv1d(n_state,
+                            n_state,
+                            kernel_size=3,
+                            stride=2,
+                            padding=1)
+        self.register_buffer("positional_embedding",
+                             sinusoids(n_ctx, n_state))
+        
+        self.blocks: Iterable[ResidualAttentionBlock] = nn.ModuleList(
+            [ResidualAttentionBlock(n_state, n_head) for _ in range(n_layer)])
+        self.ln_post = LayerNorm(n_state)
+    
+    def forward(self, x: Tensor):
+        """
+        x: torch.Tensor, shape = (batch_size, n_mels, n_ctx)
+            the mel spectrogram of the audio
+        """
+        x = F.gelu(self.conv1(x))
+        x = F.gelu(self.conv2(x))
+        x = x.permute(0, 2, 1)
+
+        assert x.shape[1:] == self.positional_embedding.shape, \
+                                "incorrect audio shape"
+        x = (x + self.positional_embedding).to(x.dtype)
+
+        for block in self.blocks:
+            x = block(x)
+        
+        x = self.ln_post(x)
+        return x
+
+
