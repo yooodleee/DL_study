7886c4a3b5016cacf8ae24505af33761fb19c575
yooodleee <accia25@naver.com>
2025-01-19 01:19:03 +0900

    transcribe an audio file using Whisper

--- whisper/whisper/transcribe.py
+++ whisper/whisper/transcribe.py
@@ -33,3 +33,36 @@ if TYPE_CHECKING:
     from .model import Whisper
 
 
+def transcribe(
+        model: "Whisper",
+        audio: Union[str, np.ndarray, torch.Tensor],
+        *,
+        verbose: Optional[bool] = None,
+        temperature: Union[float, Tuple[float, ...]] = \
+                        (0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
+        compression_ratio_threashold: Optional[float] = 2.4,
+        logprob_threshold: Optional[float] = -1.0,
+        no_speech_threshold: Optional[float] = 0.6,
+        condition_on_previous_text: bool = True,
+        initial_prompt: Optional[str] = None,
+        carry_initial_prompt: bool = False,
+        word_timestamps: bool = False,
+        prepend_punctuations: str = "\"'“¿([{-",
+        append_punctuations: str = "\"'.。,，!！?？:：”)]}、",
+        clip_timestamps: Union[str, List[float]] = 0,
+        hallucination_silence_threshold: Optional[float] = None,
+        **decode_options):
+    """
+    Transcribe an audion file using Whisper
+
+    Parameters:
+        model: Whisper
+            The Whisper model instance
+        audio: Union[str, np.ndarray, torch.Tensor]
+            The path to the audio file to open, or the audio waveform
+        verbose: bool
+            Whether to display the text being decoded to the console. If True,
+                displays all the details, If False, displays minimal details.
+                If None, does not display anything
+        
+    """
