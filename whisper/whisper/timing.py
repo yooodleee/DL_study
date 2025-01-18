2535708f45cb1ce3c8b5aa852b9c35e68d82d0bb
yooodleee <accia25@naver.com>
2025-01-18 16:39:02 +0900

    create dtw_cpu method

--- whisper/whisper/timing.py
+++ whisper/whisper/timing.py
@@ -60,7 +60,7 @@ def median_filter(x: torch.Tensor, filter_width: int):
     return result
 
 
-@numba.jit(nonpython=True)
+@numba.jit(nopython=True)
 def backtrace(trace: np.ndarray):
     i = trace.shape[0] - 1
     j = trace.shape[1] - 1
@@ -83,5 +83,31 @@ def backtrace(trace: np.ndarray):
     
     result = np.array(result)
     return result[::-1, :].T
+
+
+@numba.jit(nopython=True, parallel=True)
+def dtw_cpu(x: np.ndarray):
+    N, M = x.shape
+    cost = np.ones((N + 1, M + 1), dtype=np.float32) * np.inf
+    trace = np.ones((N + 1, M + 1), dtype=np.float32)
+
+    cost[0, 0] = 0
+    for j in range(1, M + 1):
+        for i in range(1, N + 1):
+            c0 = cost[i - 1, j - 1]
+            c1 = cost[i - 1, j]
+            c2 = cost[i, j - 1]
+
+            if c0 < c1 and c0 < c2:
+                c, t = c0, 0
+            elif c1 < c0 and c1 < c2:
+                c, t = c1, 1
+            else:
+                c, t = c2, 2
+            
+            cost[i, j] = x[i - 1, j - 1] + c
+            trace[i, j] = t
+    
+    return backtrace(trace)
 
 
