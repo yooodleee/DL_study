e3590d77015b7ebb8a0fb7bb078c07dc60ad58cc
yooodleee <accia25@naver.com>
2025-01-18 14:10:00 +0900

    update decoding.py

--- whisper/whisper/decoding.py
+++ whisper/whisper/decoding.py
@@ -766,5 +766,145 @@ class DecodingTask:
         try:
             for i in range(self.sample_len):
                 logits = self.inference.logits(tokens, audio_features)
-            
+
+                # save no_speech_probs
+                if (i == 0 and self.tokenizer.no_speech is not None):
+                    probs_at_sot = \
+                        logits[:, self.sot_index].float().softmax(dim=-1)
+                    no_speech_probs = \
+                        probs_at_sot[:, self.tokenizer.no_speech].tolist()
                 
+                # now need to consider the logits at the last token knly
+                logits = logits[:, -1]
+
+                # apply the logit filters, 
+                # e.g. for suppressing or applying penalty to
+                for logit_filter in self.logit_filters:
+                    logit_filter.apply(logits, tokens)
+                
+                # expand the tokens tensor with the selected next tokens
+                tokens, completed = \
+                    self.decoder.update(tokens, logits, sum_logprobs)
+                
+                if completed or tokens.shape[-1] > self.n_ctx:
+                    break
+        finally:
+            self.inference.cleanup_caching()
+        
+        return tokens, sum_logprobs, no_speech_probs
+    
+    @torch.no_grad()
+    def run(self, mel: Tensor) -> List[DecodingResult]:
+        self.decoder.reset()
+        tokenizer: Tokenizer = self.tokenizer
+        n_audio: int = mel.shape[0]
+
+        # encoder forward pass
+        audio_features: Tensor = self._get_audio_features(mel)  
+        tokens: Tensor = \
+            torch.tensor([self.initial_tokens]).repeat(n_audio, 1)
+        
+        # detect language if requested, overwrting the language token
+        languages, language_probs = self._detect_language(audio_features, 
+                                                            tokens)
+        if self.options.task == "lang_id":
+            return [
+                DecodingResult(audio_features=features,
+                                language=language,
+                                language_probs=probs)
+                    for features, language, probs in zip(audio_features,
+                                                        languages,
+                                                        language_probs)]
+        
+        # repeat text tensors by the group size, 
+        # for beam search or best-of-n sampling
+        tokens = tokens.repeat_interleave(self.n_group,
+                                            dim=0).to(audio_features.device)
+
+        # call the main sampling loop
+        tokens, sum_logprobs, no_speech_probs = \
+            self._main_loop(audio_features, tokens)
+        
+        # reshape the tensors to have (n_audio, n_group) 
+        # as the first two dimensions
+        audio_features = audio_features[:: self.n_group]
+        no_speech_probs = no_speech_probs[:: self.n_group]
+        assert audio_features.shape[0] == len(no_speech_probs) == n_audio
+
+        tokens = tokens.reshape(n_audio, self.n_group, -1)
+        sum_logprobs = sum_logprobs.reshape(n_audio, self.n_group)
+
+        # get the final candidates for each group, and slice between the first
+        # sampled token and EOT
+        tokens, sum_logprobs = self.decoder.finalize(tokens, sum_logprobs)
+        tokens: List[List[Tensor]] = [
+            [t[self.sample_begin : (t == tokenizer.eot).nonzero()[0, 0]] 
+            for t in s] for s in tokens
+        ]
+
+        # select the top-ranked sample in each group
+        selected = self.sequence_ranker.rank(tokens, sum_logprobs)
+        tokens: List[List[int]] = [t[i].tolist() 
+                                    for i, t in zip(selected, tokens)]
+        texts: List[str] = [tokenizer.decode(t).strip() for t in tokens]
+
+        sum_logprobs: List[float] = [lp[i] 
+                                    for i, lp in zip(selected, sum_logprobs)]
+        avg_logprobs: List[float] = [lp / (len(t) + 1) 
+                                    for t, lp in zip(tokens, sum_logprobs)]
+
+        fields = (texts, 
+                  languages, 
+                  tokens, 
+                  audio_features, 
+                  avg_logprobs, 
+                  no_speech_probs)
+        if len(set(map(len, fields))) != 1:
+            raise RuntimeError(
+                f"inconsistent result lengths: {list(map(len, fields))}")
+        
+        return [
+            DecodingResult(audio_features=features,
+                            language=language,
+                            tokens=tokens,
+                            text=text,
+                            avg_logprob=avg_logprob,
+                            no_speech_prob=no_speech_prob,
+                            temperature=self.options.temperature,
+                            compression_ratio=compression_ratio(text))
+
+            for text, language, tokens, features, \
+                avg_logprob, no_speech_prob in zip(*fields)]
+    
+    @torch.no_grad()
+    def decode(
+            model: "Whisper",
+            mel: Tensor,
+            options: DecodingOptions = DecodingOptions(),
+            **kwargs) -> Union[DecodingResult, List[DecodingResult]]:
+        """
+        Performs decoding of 30-second audio segment(s), provided as Mel spectrogram(s).
+
+        Parameters:
+            model(Whisper)
+                the Whisper model instance
+            mel(torch, shape = (80, 3000) or (*, 80, 3000))
+                A tensor containing the Moel spectrogram(s)
+            Options(DecodingOptions)
+                A dataclass that contains all necessary options for decoding
+                    30-second segments
+
+        Returns:
+            result(Union[Decodingresult, List[DecodingResult]])
+                The result(s) of decoding contained in 'DecodingResult' 
+                    dataclass instance(s).
+        """
+        if single := mel.ndim == 2:
+            mel = mel.unsqueeze(0)
+        
+        if kwargs:
+            options = replace(options, **kwargs)
+        
+        result = DecodingTask(model, options).run(mel)
+
+        return result[0] if single else result
