bd604292ae424f4c0b789ee8952979458151f614
yooodleee <accia25@naver.com>
2025-01-18 15:26:17 +0900

    create TextDecoder class

--- whisper/whisper/model.py
+++ whisper/whisper/model.py
@@ -243,3 +243,55 @@ class AudioEncoder(nn.Module):
         return x
 
 
+class TextDecoder(nn.Module):
+    def __init__(
+            self,
+            n_vocab: int,
+            n_ctx: int,
+            n_state: int,
+            n_head: int,
+            n_layer: int):
+        super().__init__()
+
+        self.token_embedding = nn.Embedding(n_vocab, n_state)
+        self.positional_embedding = nn.Parameter(torch.empty(n_ctx, n_state))
+
+        self.blocks: Iterable[ResidualAttentionBlock] = nn.ModuleList(
+            [
+                ResidualAttentionBlock(n_state, n_head, cross_attention=True)
+            ]
+        )
+        self.ln = LayerNorm(n_state)
+
+        mask = torch.empty(n_ctx, n_ctx).fill_(-np.inf).triu_(1)
+        self.register_buffer("mask", mask, persistent=False)
+    
+    def forward(
+            self,
+            x: Tensor,
+            xa: Tensor,
+            kv_cache: Optional[dict] = None):
+        """
+        x: torch.LongTensor, shape = (batch_size, <= n_ctx)
+            the text tokens
+        xa: torch.Tensor, shape = (batch_size, n_audio_ctx, n_audio_state)
+            the encoded audio features to be attended on
+        """
+        offset = next(iter(kv_cache.values())).shape[1] if kv_cache else 0
+        x = (self.token_embedding(x)
+             + self.positional_embedding[offset : offset + x.shape[-1]])
+        x = x.to(xa.dtype)
+
+        for block in self.blocks:
+            x = block(x,
+                      xa,
+                      mask=self.mask,
+                      kv_cache=kv_cache)
+        
+        x = self.ln(x)
+        logits = (x @ torch.transpose(self.token_embedding.weight.to(x.dtype),
+                                      0, 1).float())
+        
+        return logits
+
+
