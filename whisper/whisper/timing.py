172628c15a2681f072aab29dcb606f59d5be7efa
yooodleee <accia25@naver.com>
2025-01-18 16:32:58 +0900

    apply a median filter of width 'filter_width' along the last dimension

---
+++ whisper/whisper/timing.py
@@ -0,0 +1,62 @@
+import itertools
+import subprocess
+import warnings
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, List
+
+import numba
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from .audio import HOP_LENGTH, SAMPLE_RATE, TOKENS_PER_SECOND
+from .tokenizer import Tokenizer
+
+if TYPE_CHECKING:
+    from .model import Whisper
+
+
+def median_filter(x: torch.Tensor, filter_width: int):
+    """
+    Apply a median filter of width `filter_width` along the last dimension
+        of `x`
+    """
+    pad_width = filter_width // 2
+    if x.shape[-1] <= pad_width:
+        # F.pad requires the padding with to be smaller than the 
+        # input dimension
+        return x
+    
+    if (ndim := x.ndim) <= 2:
+        # `F.pad` does not support 1D or 2D inputs for reflext padding
+        # but supports 3D and 4D
+        x = x[None, None, :]
+    
+    assert (filter_width > 0 and filter_width % 2 == 1), \
+    "`filter_width` should be an odd number"
+
+    result = None
+    x = F.pad(x, (filter_width // 2, filter_width // 2, 0, 0), mode="reflect")
+    if x.is_cuda:
+        try:
+            from .train_ops import media_filter_cuda
+
+            result = media_filter_cuda(x, filter_width)
+        except (RuntimeError, subprocess.CalledProcessError):
+            warnings.warn(
+                "Failed to launch Triton kernels, likely due to missing CUDA "
+                "toolkit; falling back to a slower median kernel "
+                "implementation...")
+    
+    if result is None:
+        # sort() is faster than torch.median
+        # (https://github.com/pytorch/pytorch/issues/51450)
+        result = x.unfold(-1, 
+                          filter_width, 1).sort()[0][..., filter_width // 2]
+    
+    if ndim <= 2:
+        result = result[0, 0]
+    
+    return result
+
+
