9ddd8889bafcad63ab659017f2ce8ba812fc4266
yooodleee <accia25@naver.com>
2025-01-19 01:11:42 +0900

    create transcribe.py

---
+++ whisper/whisper/transcribe.py
@@ -0,0 +1,35 @@
+import argparse
+import os
+import traceback
+import warnings
+from typing import TYPE_CHECKING, List, Optional, Tuple, Union
+
+import numpy as np
+import torch
+import tqdm
+
+from .audio import (
+    FRAMES_PER_SECOND,
+    HOP_LENGTH,
+    N_FRAMES,
+    N_SAMPLES,
+    log_mel_spectrogram,
+    pad_or_trim)
+
+from .decoding import DecodingOptions, DecodingResult
+from .timing import add_word_timestapms
+from .tokenizer import LANGUAGES, TO_LANGUAGE_CODE, get_tokenizer
+from .utils import (
+    exact_div,
+    format_timestamp,
+    get_end,
+    get_writer,
+    make_safe,
+    optional_float,
+    optional_int,
+    str2bool)
+
+if TYPE_CHECKING:
+    from .model import Whisper
+
+
