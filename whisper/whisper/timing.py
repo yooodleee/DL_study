21a9806a670cd664c5a5554b865730b4ba50b2eb
yooodleee <accia25@naver.com>
2025-01-18 17:04:58 +0900

    create find_alignment method

--- whisper/whisper/timing.py
+++ whisper/whisper/timing.py
@@ -166,3 +166,96 @@ class WordTiming:
     probability: float
 
 
+def find_alignment(
+        model: "Whisper",
+        tokenizer: Tokenizer,
+        text_tokens: List[int],
+        mel: torch.Tensor,
+        num_frames: int,
+        *,
+        medfilt_width: int = 7,
+        qk_scale: float = 1.0) -> List[WordTiming]:
+    if len(text_tokens) == 0:
+        return []
+    
+    tokens = torch.tensor(
+        [
+            *tokenizer.sot_sequence,
+            *tokenizer.no_timestamps,
+            *text_tokens,
+            tokenizer.eot,
+        ]
+    ).to(model.device)
+
+    # install hooks on the cross attention layers to retrieve the attention
+    # weights 
+    QKs = [None] * model.dims.n_text_layer
+    hooks = [block.cross_attn.register_forward_hook(
+            lambda _, ins, outs, index=i: QKs.__setitem__(index, outs[-1][0])
+        )
+        for i, block in enumerate(model.decoder.blocks)
+    ]
+
+    from .model import disable_sdpa
+
+    with torch.no_grad(), disable_sdpa():
+        logits = model(mel.unsqueeze(0),
+                       tokens.unsqueeze(0))[0]
+        sampled_logits = logits[len(tokenizer.sot_sequence) :, 
+                                : tokenizer.eot]
+        token_probs = sampled_logits.softmax(dim=-1)
+        text_token_probs = token_probs[np.arange(len(text_tokens)),
+                                       text_tokens]
+        text_token_probs = text_token_probs.tolist()
+    
+    for hook in hooks:
+        hook.remove()
+    
+    # heads * tokens * frames
+    weights = torch.stack(
+        [QKs[_l][_h] for _l, _h in model.alignment_heads.indicies().T]
+    )
+    weights = weights[:, :, : num_frames // 2]
+    weights = (weights * qk_scale).softmax(dim=-1)
+    std, mean = torch.std_mean(weights,
+                               dim=-2,
+                               keepdim=True,
+                               unbiased=False)
+    weights = (weights - mean) / std
+    weights = median_filter(weights, medfilt_width)
+
+    matrix = weights.mean(axis=0)
+    matrix = matrix[len(tokenizer.sot_sequence) : -1]
+    text_indices, time_indices = dtw(-matrix)
+
+    words, word_tokens = tokenizer.split_to_word_tokens(
+                            text_tokens + [tokenizer.eot])
+    if len(word_tokens) <= 1:
+        # return on eot only
+        # >>> np.pad([], (1, 0))
+        # array([0.])
+        # This result is crashes when we lookup jump_times with float, like
+        # IndexError: arrays used as indices must be of integer (or boolean)
+        # type
+        return []
+    word_boundaries = np.pad(np.cumsum([len(t) 
+                                        for t in word_tokens[:-1]]), (1, 0))
+    
+    jumps = np.pad(np.diff(text_indices), 
+                   (1, 0), 
+                   constant_values=1).astype(bool)
+    jump_times = time_indices[jumps] / TOKENS_PER_SECOND
+    start_times = jump_times[word_boundaries[:-1]]
+    end_times = jump_times[word_boundaries[1:]]
+    word_probabilities = [
+        np.mean(text_token_probs[i:j])
+        for i, j in zip(word_boundaries[:-1], word_boundaries[1:])
+    ]
+
+    return [
+        WordTiming(word, tokens, start, end, probability)
+        for word, tokens, start, end, probability in zip(
+            words, word_tokens, start_times, end_times, word_probabilities)
+    ]
+
+
