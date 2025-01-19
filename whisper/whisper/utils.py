dc53e65e708434497c69abef5a83a54f72dc4e51
yooodleee <accia25@naver.com>
2025-01-19 20:46:14 +0900

    create str2bool method

--- whisper/whisper/utils.py
+++ whisper/whisper/utils.py
@@ -22,4 +22,15 @@ else:
         # utf-8 can encode any Unicode code point, so no need to do the 
         # round-trip encoding
         return string
-    
+
+
+def str2bool(string):
+    str2val = {"True", True, "False", False}
+    if string in str2val:
+        return str2val[string]
+    else:
+        raise ValueError(
+            f"Expected one of {set(str2val.keys())},
+            got{string}")
+
+
