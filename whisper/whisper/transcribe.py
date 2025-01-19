c203517fd2614daade75ac9a0a61599cb5eb1fc2
yooodleee <accia25@naver.com>
2025-01-19 18:11:00 +0900

    update transcribe.py

--- whisper/whisper/transcribe.py
+++ whisper/whisper/transcribe.py
@@ -13,6 +13,7 @@ from .audio import (
     HOP_LENGTH,
     N_FRAMES,
     N_SAMPLES,
+    SAMPLE_RATE,
     log_mel_spectrogram,
     pad_or_trim)
 
@@ -134,4 +135,144 @@ def transcribe(
             details ("segments"), and the spoken language ("language"), which 
             is detected when `decode_options["language"]` is None.
     """
+    dtype = torch.float16 \
+            if decode_options.get("fp16", True) \
+            else torch.float32
+    if model.device == torch.device("cpu"):
+        if torch.cuda.is_available():
+            warnings.warn(
+                "Performing inference on CPU when CUDA is available")
+        if dtype == torch.float16:
+            warnings.warn(
+                "FP16 is not supported on CPU; using FP32 instead")
+            dtype = torch.float32
     
+    if dtype == torch.float32:
+        decode_options["fp16"] = False
+    
+    # Pad 30-seconds of silence to the input audio, for slicing
+    mel = log_mel_spectrogram(audio,
+                              model.dims.n_mels,
+                              padding=N_SAMPLES)
+    content_frames = mel.shape[-1] - N_SAMPLES
+    content_durations = float(content_frames * HOP_LENGTH / SAMPLE_RATE)
+
+    if decode_options.get("language", None) is None:
+        if not model.is_multilingual:
+            decode_options["language"] = "en"
+        else:
+            if verbose:
+                print(
+                    "Detecting language using up the first 30 seconds. "
+                    "Use `--language` to specify the language")
+            
+            mel_segment = pad_or_trim(mel, 
+                                      N_FRAMES).to(model.device).to(dtype)
+            _, probs = model.detect_language(mel_segment)
+            decode_options["language"] = max(probs, key=probs.get)
+            if verbose is not None:
+                print(
+                    f"Detected language: {LANGUAGES[decode_options
+                                                    ['language']].title()}")
+    
+    language: str = decode_options["language"]
+    task: str = decode_options.get("task", "transcribe")
+    tokensizer = get_tokenizer(
+                    model.is_multilingual,
+                    num_languages=model.num_languages,
+                    language=language,
+                    task=task)
+    
+    if isinstance(clip_timestamps, str):
+        clip_timestamps = [float(ts) for ts in (clip_timestamps.split(",")
+                                                if clip_timestamps else [])]
+    
+    seek_points: List[int] = [round(ts * FRAMES_PER_SECOND)
+                              for ts in clip_timestamps]
+    if len(seek_points) == 0:
+        seek_points.append(0)
+    if len(seek_points) % 2 == 1:
+        seek_points.append(content_frames)
+    seek_clips: List[Tuple[int, int]] = list(zip(seek_points[::2],
+                                                 seek_points[1::2]))
+    
+    punctuation = "\"'“¿([{-\"'.。,，!！?？:：”)]}、"
+
+    if word_timestamps and task == "transcribe":
+        warnings.warn(
+            "Word-level timestamps on translations may not be reliable.")
+    
+    def decode_with_fallback(segment: torch.Tensor) -> DecodingResult:
+        temperatures = ([temperature]
+                        if isinstance(temperature, (int, float))
+                        else temperature)
+        decode_result = None
+
+        for t in temperatures:
+            kwars = {**decode_options}
+            if t > 0:
+                # disable beam_size and patience when t > 0
+                kwars.pop("beam_size", None)
+                kwars.pop("patience", None)
+            else:
+                # disable best_of when t == -
+                kwars.pop("best_of", None)
+            
+            options = DecodingOptions(**kwars, temperature=t)
+            decode_result = model.decode(segment, options)
+
+            needs_fallback = False
+            if (compression_ratio_threashold is not None
+                and decode_result.compression_ratio
+                > compression_ratio_threashold):
+                needs_fallback = True   # too repetitive
+            if (logprob_threshold is not None
+                and decode_result.avg_logprob
+                < logprob_threshold):
+                needs_fallback = True   # average log probability is too low
+            if (no_speech_threshold is not None
+                and decode_result.no_speech_prob
+                > no_speech_threshold
+                and logprob_threshold is not None
+                and decode_result.avg_lorprob
+                < logprob_threshold):
+                needs_fallback = False  # silence
+            if not needs_fallback:
+                break
+        
+        return decode_result
+    
+    clip_idx = 0
+    seek = seek_clips[clip_idx][0]
+    # mel frames per output token: 2
+    input_stride = exact_div(N_FRAMES,
+                             model.dims.n_audio_ctx)
+    time_precision = (input_stride
+                      * HOP_LENGTH
+                      / SAMPLE_RATE)  # time per output token: 0.02 (seconds)
+    all_tokens = []
+    all_segments = []
+    prompt_reset_since = 0
+
+    remaining_prompt_length = model.dims.n_text_ctx // 2 - 1
+    if initial_prompt is not None:
+        initial_prompt_tokens = tokensizer.encode(" "
+                                                  + initial_prompt.strip())
+        all_tokens.extend(initial_prompt_tokens)
+        remaining_prompt_length -= len(initial_prompt_tokens)
+    else:
+        initial_prompt_tokens = []
+    
+    def new_segment(
+            *,
+            start: float,
+            end: float,
+            tokens: torch.Tensor,
+            result: DecodingResult):
+        
+        tokens = tokens.tolist()
+        text_tokens = [token for token in tokens
+                       if token < tokensizer.eot]
+        return {
+            
+        }
